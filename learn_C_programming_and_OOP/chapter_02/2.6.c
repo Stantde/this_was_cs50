c28f91396c9ece73b7171d821256dca25cb2d7c9
Stantde <125204126+Stantde@users.noreply.github.com>
2025-11-24 03:34:42 -0600

    Implement wordlength function in 2.6.c
    Added a function to compute the word length of the host machine.

--- learn_C_programming_and_OOP/chapter_02/2.6.c
+++ learn_C_programming_and_OOP/chapter_02/2.6.c
@@ -1,4 +1,28 @@
-modified_getbits(unsigned x, int p, int n){
-int b = 32; // b represents the number of bits in an unsigned int. At this point, assume 32.
-return (( x >> (( b - 1 - p)-( n - 1 ))) & ~( ~0 << n ))
+#include <stdio.h>
+/*
+Exercise 2-6. Write a function wordlength() which computes the word length of
+the host machine, that is, the number of bits in an int. The function should be
+portable, in the sense that the same source code works on all machines.
+*/
+int wordlength(void);
+int main() {
+    int a;
+    a=wordlength();
+    printf("%i\n", a);
+
+    return 0;
+}
+int wordlength(void){
+/*
+*/
+    unsigned int bit_tracker=1;
+    int count=0;
+
+    while(bit_tracker){
+        bit_tracker=bit_tracker<<1;
+        ++count;
+        //printf("%i\n", count);
+        //printf("%b\n", bit_tracker);
+    }
+    return count;
 }
