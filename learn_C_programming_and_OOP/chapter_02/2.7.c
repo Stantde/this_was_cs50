1fe2f638a864abdc2e4cb61052420ff6ffc6f743
Stantde <125204126+Stantde@users.noreply.github.com>
2025-11-27 13:56:12 -0600

    Fix right rotation and update main function

--- learn_C_programming_and_OOP/chapter_02/2.7.c
+++ learn_C_programming_and_OOP/chapter_02/2.7.c
@@ -5,8 +5,8 @@ Exercise 2-7. Write the function rightrot(n, b) which rotates the integer n to t
 int wordlength(void);
 int rightrot(unsigned int n, int b);
 int main() {
-    int a;
-    printf("%i\n", a);
+    int a=rightrot(0x1A, 3);
+    printf("%X\n", a);
 
     return 0;
 }
@@ -19,14 +19,14 @@ int wordlength(void){
     while(bit_tracker){
         bit_tracker=bit_tracker<<1;
         ++count;
-        printf("%i\n", count);
-        printf("%b\n", bit_tracker);
+        //printf("%i\n", count);
+        //printf("%b\n", bit_tracker);
     }
     return count;
 }
 int rightrot(unsigned int n, int b){
-    int a;
-    a=wordlength();
-  
-    return 0;
+    int tmp=n&~(~0<<b);
+    int diff=wordlength()-b;
+    return ((n>>3)|(tmp<<diff));
+
 }
